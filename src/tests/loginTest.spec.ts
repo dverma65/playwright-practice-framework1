0ef3685788ad41fa00812112abd82fa07d0330b3
Deeksha <deekshaverma65@gmail.com>
2026-08-28 12:12:47 +0530

    Cryptojs integration

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,15 +1,23 @@
 import { test } from '@playwright/test';
 import LoginPage from '../pages/LoginPage';
 
-test("Login Page test", async({page}) =>{
+test.skip("Login Page test", async({page}) =>{
     const loginPage = new LoginPage(page);
 
     await loginPage.navigateToLoginPage();
-    await loginPage.fillUserName("deekshaverma6509@gmail.com");
+    //await loginPage.fillUserName("deekshaverma6509@gmail.com");
+    await loginPage.fillUserName(process.env.userid!);
     await loginPage.clickLoginBtn();
-    await loginPage.fillPassword("Deeksh@1308");
+    //await loginPage.fillPassword("Deeksh@1308");
+    await loginPage.fillPassword(process.env.password!)
     const homePage = await loginPage.clickLoginBtn();
     await page.waitForTimeout(7000);
     await homePage.expectServiceTitleToBeVisible();
     
-})
+});
+
+test("sample test", async({page}) => {
+    console.log(process.env.NODE_ENV);
+    console.log(process.env.userid);
+    console.log(process.env.password);
+});
